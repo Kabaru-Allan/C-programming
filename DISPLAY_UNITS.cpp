a1b37b9f49b937248878efc5a41ca59ced1da0c8
Kabaru -Allan <kamauallan923@gmail.com>
2025-09-30 13:09:27 +0300

    Add files via upload

---
+++ DISPLAY_UNITS.cpp
@@ -0,0 +1,19 @@
+/*
+NAME:KABARU ALLAN KAMAU
+REG:PA106/G/28220/25
+DESCRIPTION:DISPLAY_UNITS
+*/
+
+#include<stdio.h>
+//main fuction
+int main(){
+	//display_units
+	printf("units offered this semester:\n");
+	printf("1.Introduction to Programming\n");
+	printf("2.Data Structure\n");
+	printf("3.Computer Networks\n");
+	printf("4.Database Systems\n");
+	printf("5.Software Engineering\n");
+	
+	return 0;
+}
