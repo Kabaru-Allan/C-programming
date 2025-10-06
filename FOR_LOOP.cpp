f4eac7b4378c55757bd5e44f8ed61deb5bcf2801
Kabaru -Allan <kamauallan923@gmail.com>
2025-10-06 11:34:21 +0300

    Add files via upload

---
+++ FOR_LOOP.cpp
@@ -0,0 +1,17 @@
+/*
+NAME:KABARU ALLAN KAMAU
+REG:PA106/G/28220/25
+DESCRIPTION:LOOP TO PRINT NUMBERS FROM 100-50 IN DESCENDING ORDER
+DATE:06/10/2025
+*/
+
+#include<stdio.h>
+
+int main(){
+
+	int i;
+	for(i=100;i>=50;i--){
+		printf("%d\n",i);
+	}
+	return 0;
+}
